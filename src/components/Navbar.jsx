3bb13d57864fa68fe2d01872724fd2d9ee4adc29
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2024-12-13 17:03:23 +0200

    adding navbar to the apphome page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Logo Section */}
+      <div className="logo">
+        <span>Sportofolio</span>
+      </div>
+
+      {/* Navbar Links */}
+      <ul className="navbar-links">
+        <li><a href="#foryou">For You</a></li>
+        <li><a href="#network">Network</a></li>
+        <li><a href="#messages">Messages</a></li>
+        <li><a href="#opportunities">Opportunities</a></li>
+      </ul>
+
+      {/* Profile Section */}
+      <div className="profile">
+        <img src=".\src\assets\profile pic.png" alt="Profile" /> {/* Placeholder image */}
+        <div className="profile-text">
+          <p className="greeting">Hello,</p>
+          <p className="name">Zeyad Waleed</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
