2987df96140ddb699129836f6928a2a9d18dbe28
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2024-12-14 10:55:53 +0200

    home changes (for you - following )

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,19 @@
-// src/components/Navbar.jsx
 import React from 'react';
-import './Navbar.css'; // Import the CSS file for styling
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Logo Section */}
       <div className="logo">
         <img src="./src/assets/logo.png" alt="Sportofolio Logo" className="logo-img" />
       </div>
 
-      {/* Navbar Links */}
       <ul className="navbar-links">
         <li><a href="#foryou">For You</a></li>
-        <li><a href="#network">Network</a></li>
-        <li><a href="#messages">Messages</a></li>
-        <li><a href="#opportunities">Opportunities</a></li>
+        <li><a href="#network">Following</a></li>
       </ul>
-
-      {/* Profile Section */}
       <div className="profile">
-        <img src=".\src\assets\profile pic.png" alt="Profile" /> {/* Placeholder image */}
+        <img src=".\src\assets\profile pic.png" alt="Profile" />
         <div className="profile-text">
           <p className="greeting">Hello,</p>
           <p className="name">Zeyad Waleed</p>
