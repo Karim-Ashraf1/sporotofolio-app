5f56ccb7fd1de4409b4f737896470ef4d5e84b90
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2024-12-14 11:35:43 +0200

    Adding search bar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import './Navbar.css'; 
+import './Navbar.css';
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo">
-        <img src="./src/assets/logo.png" alt="Sportofolio Logo" className="logo-img" />
+      <div className="logo-search-container">
+        <div className="logo">
+          <img
+            src="./src/assets/logo.png"
+            alt="Sportofolio Logo"
+            className="logo-img"
+          />
+        </div>
+        <SearchBar />
       </div>
 
       <ul className="navbar-links">
-        <li><a href="#foryou">For You</a></li>
-        <li><a href="#network">Following</a></li>
+        <li>
+          <a href="#foryou">For You</a>
+        </li>
+        <li>
+          <a href="#network">Following</a>
+        </li>
       </ul>
       <div className="profile">
-        <img src=".\src\assets\profile pic.png" alt="Profile" />
+        <img src="./src/assets/profile pic.png" alt="Profile" />
         <div className="profile-text">
           <p className="greeting">Hello,</p>
           <p className="name">Zeyad Waleed</p>
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBar.css';
+import { FaSearch } from 'react-icons/fa';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <FaSearch className="search-icon" />  
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-input"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
